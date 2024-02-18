bd05d45b5df96444b172dc7a26bdd3f291362f4c
Melnikov Dmitry <dm.melnikau@gmail.com>
2024-02-18 23:54:34 +0300

    task#

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService:IOrderService
+    {
+        public IEnumerable<Order> Orders { get; private set; }
+        public async Task<Order> GetOrder()
+        {
+            return Orders.OrderByDescending(u => u.Price).FirstOrDefault();
+        }
+
+        public async  Task<List<Order>> GetOrders()
+        {
+            List<Order> orders = Orders.Where(u => u.Quantity>10).ToList();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,24 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Enums;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService:IUserService
+    {
+        public IEnumerable<User> Users { get; private set; }
+
+        public async Task<User> GetUser()
+        {
+            
+            return Users.OrderByDescending(u => u.Orders.Count).FirstOrDefault();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            List<User> inactiveUsers = Users.Where(u => u.Status == UserStatus.Inactive).ToList();
+
+            return inactiveUsers;
+        }
+    }
+}
